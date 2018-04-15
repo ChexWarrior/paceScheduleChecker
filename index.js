ba2dcb49c5fb22881c4129899f7848dbf66bc57f
Chexwarrior <aplehm@gmail.com>
2018-04-15 10:22:22 -0400

    Select proper vlaues

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const puppeteer = require('puppeteer');
-
 const scheduleURL = 'https://appsrv.pace.edu/ScheduleExplorerLive/index.cfm';
+const submitSelector = '#submitbutton';
 
 (async () => {
   const browser = await puppeteer.launch({
     executablePath: '/usr/bin/google-chrome'
   });
+
   const page = await browser.newPage();
   await page.setViewport({
     width: 1280,
     height: 800
   });
+
   await page.goto(scheduleURL);
+
+  await page.waitForFunction(function() {
+    let semesterDropdown = document.querySelector('#checkterm');
+    let studentTypeDropdown = document.querySelector('#level');
+    let subjectDropdown = document.querySelector('#subject');
+
+    // fall 2018
+    semesterDropdown.value = '201870';
+    studentTypeDropdown.value = 'Graduate';
+    subjectDropdown.value = 'CS';
+
+    return true;
+  });
+
+  // const submitHandle = await page.$(submitSelector);
+  // await submitHandle.click();
+
   await page.screenshot({
     path: 'example.png'
   });
