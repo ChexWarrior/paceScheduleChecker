9e572cb80ca0daa862e298148d1f3b280a77e22c
Andrew Lehman <aplehm@gmail.com>
2018-11-01 19:56:45 -0400

    Return html from schedule request

---
+++ main.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\DomCrawler\Crawler;
+use GuzzleHttp\Client;
+
+const SCHEDULE_URL = 'https://appsrv.pace.edu/ScheduleExplorerLive/index.cfm';
+
+$semester = $argv[1];
+$level = $argv[2];
+$major = $argv[3];
+$course = $argv[4];
+
+$client = new Client();
+$response = $client->request('POST', SCHEDULE_URL, [
+  'form_params' => [
+    'level' => $level,
+    'term' => $semester,
+    'subject' => $major,
+  ],
+]);
+
+$body = $response->getBody();
+
+echo $body;
